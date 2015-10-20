c690d6543da83a9382f9967d83e7acb49634e9ba
Nayana R V <nayanarv90@gmail.com>
2015-10-20 14:49:07 +0530

    Initial Commit

---
+++ sampleLayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleLayer
+//
+//  Created by Nayana on 20/10/15.
+//  Copyright © 2015 Nayana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
